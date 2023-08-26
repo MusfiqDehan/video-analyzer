dd325a7ed059e3300af76e321f2053271645b77b
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-08-26 17:49:52 +0600

    Update upload folder path

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.ERROR)
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'static/uploads')
 
 
 @app.route('/', methods=['GET', 'POST'])
